c6c178216b1780b395dfb71fcec44f2dddf69924
Mateus S. Oliveira <devmateusoliveira@gmail.com>
2025-02-01 11:51:50 -0300

    feat: second challenge create random id

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -19,6 +21,20 @@ func main() {
 
 		// Update the message type to return back.
 		body["type"] = "echo_ok"
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+		body["id"] = time.Now().UTC().UnixNano() + int64(rand.Int())
 
 		// Echo the original message back with the updated message type.
 		return n.Reply(msg, body)
